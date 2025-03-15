c6933bded55846cc3a931bc3397e33d86d55ae9b
BoinkGit <rmelyle@gmail.com>
2025-03-15 02:53:05 -0400

    added health bar

--- main/src/main.py
+++ main/src/main.py
@@ -16,20 +16,30 @@ class player:
         if not image is None:
             pass
         else:
-            draw_rectangle(self.x,self.y,self.w,self.h,RED)
+            draw_rectangle_v(Vector2(self.x-self.w/2,self.y-self.h/2),Vector2(self.w,self.h),RED)
 
 
 init_window(750,750,">///< s-senpai don't touch me there")
 set_target_fps(60)
 
-player1 = player(32,653,20,20,10,3)
+player1 = player(375,375,20,20,10,3)
 
 while not window_should_close():
     begin_drawing()
     clear_background(WHITE)
     
+    ## player
     player1.move(KEY_LEFT,KEY_RIGHT,KEY_UP,KEY_DOWN)
     player1.draw()
+
+    if is_key_pressed(KEY_SPACE):
+        player1.hp -= 1
+        
+
+    ## health bar
+    for i in range(player1.hp):
+        draw_rectangle(20,20+30*i,20,20,PINK)
+
 
 
     end_drawing()
